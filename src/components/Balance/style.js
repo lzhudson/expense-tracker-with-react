74994ad4c5dacc53c729a3681e5e87227f0322bd
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-22 09:14:36 -0300

    feat: create style Balance component

---
+++ src/components/Balance/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const BalanceTitle = styled.h4`
+  text-transform: uppercase;
+`;
+
+export const BalanceTextValue = styled.h1`
+  letter-spacing: 1px;
+  margin: 0;
+`;
