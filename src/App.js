dd144328cd1ac9c28b13ab038ab4601f20a96f3e
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-22 09:02:01 -0300

    feat: add Header component in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
