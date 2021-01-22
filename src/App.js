87e1581ed84b47cf376579183ef68d75cbebe181
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-22 09:02:33 -0300

    feat: add global style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
       <Header />
+      <GlobalStyle />
     </>
   );
 }
