ddaee83847f6d052425f60a5a735ba05088e477a
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-24 14:27:31 -0300

    feat: create structure AddTransaction component

---
+++ src/components/AddTransaction/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FormTransaction, FormControl } from './styles';
+
+export default function AddTransaction() {
+  return (
+    <>
+      <h3>Add Transaction</h3>
+      <FormTransaction>
+        <FormControl>
+          <label htmlFor="text">Text</label>
+          <input type="text" placeholder="Enter text..." id="text" />
+        </FormControl>
+        <FormControl>
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input type="number" placeholder="Enter amount..." id="amount" />
+        </FormControl>
+        <button type="submit">Add Transaction</button>
+      </FormTransaction>
+    </>
+  );
+}
