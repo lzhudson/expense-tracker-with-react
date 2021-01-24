6ba7561392689c0b5a9f7c3852f3f4bb0694c1d2
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-24 14:27:13 -0300

    feat: add AddTransaction component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import GlobalStyle from './styles/global';
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </Container>
       <GlobalStyle />
     </>
