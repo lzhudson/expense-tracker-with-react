37bf9d338b625fdc538a48ac6bab14dad4822523
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-26 17:07:53 -0300

    feat: show total transactions in Balance

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BalanceTitle, BalanceTextValue } from './style';
+import { GlobalContext } from '../../context/GlobalState';
+import { formatPrice } from '../../util/format';
 
 export default function Balance() {
+  const { transactions } = useContext(GlobalContext);
+  const totalBalance = transactions.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0
+  );
   return (
     <>
       <BalanceTitle>Your Balance</BalanceTitle>
-      <BalanceTextValue>$0.00</BalanceTextValue>
+      <BalanceTextValue>{formatPrice(totalBalance)}</BalanceTextValue>
     </>
   );
 }
