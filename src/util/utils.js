2ac61d9463d6b56131ccc9ce8015d81b00e2b686
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-26 17:27:28 -0300

    feat: create function to filter transactions by type

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,3 +1,16 @@
 export function typeTransaction(amount) {
   return amount < 0 ? 'minus' : 'plus';
 }
+export function filterTransactionsByType(transactions, type) {
+  let transactionsFiltered = [];
+  if (type === 'income')
+    transactionsFiltered = transactions.filter((transaction) =>
+      transaction.amount < 0 ? transaction : null
+    );
+  if (type === 'expense')
+    transactionsFiltered = transactions.filter((transaction) =>
+      transaction.amount > 0 ? transaction : null
+    );
+
+  return transactionsFiltered;
+}
