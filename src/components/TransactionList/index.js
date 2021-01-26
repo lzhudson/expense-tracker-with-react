2e104e8c8c683c5a4b6510bd3d81c70151ca27b1
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-26 17:02:32 -0300

    feat: use global state to show transactions

--- src/components/TransactionList/index.js
+++ src/components/TransactionList/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { List, ListItem } from './styles';
+import { GlobalContext } from '../../context/GlobalState';
+import { formatPrice } from '../../util/format';
+import { typeTransaction } from '../../util/utils';
 
 export default function TransactionList() {
+  const { transactions } = useContext(GlobalContext);
+  const transactionList = transactions.map((transaction) => ({
+    ...transaction,
+    priceFormatted: formatPrice(transaction.amount),
+    typeTransaction: typeTransaction(transaction.amount),
+  }));
   return (
     <>
       <h3>History</h3>
       <List>
-        <ListItem type="plus">
-          <div>
-            <span>Cash</span>
-            <span>+$400</span>
-          </div>
-          <button type="button">X</button>
-        </ListItem>
-        <ListItem type="minus">
-          <div>
-            <span>Cash</span>
-            <span>-$400</span>
-          </div>
-          <button type="button">X</button>
-        </ListItem>
+        {transactionList.map((transaction) => (
+          <ListItem key={transaction.id} type={transaction.typeTransaction}>
+            <div>
+              <span>{transaction.text}</span>
+              <span>{transaction.priceFormatted}</span>
+            </div>
+            <button type="button">X</button>
+          </ListItem>
+        ))}
       </List>
     </>
   );
