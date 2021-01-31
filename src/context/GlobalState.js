eccb5b6cce313492b976e8f9bc80aa37b4aabee6
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-31 19:55:20 -0300

    feat: add action deleteTransaction

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -14,6 +14,13 @@ export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    });
+  }
   return (
     <GlobalContext.Provider
       value={{
